06fd314f22183c7812ca4bd61288a7b6d0d89294
Palash Khandelwal <80159219+PalashKhandelwal@users.noreply.github.com>
2024-06-25 15:13:08 +0530

    front end initial

---
+++ hotelappFrontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Rooms from "./Rooms";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Rooms/>
+    </div>
+  );
+}
+
+export default App;
